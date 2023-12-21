151d5521bfcda3cf3b540cf9c0fc06832414d625
Fabien LIERVILLE <fabien.lierville27@gmail.com>
2023-12-21 11:51:05 +0100

    TetField + CheckBox + Radio

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -23,6 +23,8 @@ class _HomePageState extends State<HomePage> {
   //RadioBox
   RadioChoix choixRadio = RadioChoix.Avion;
   Icon iconRadio = Icon(Icons.airplanemode_active);
+  //Switch
+  bool interrupteur = false;
 
   @override
   void initState() {
@@ -51,92 +53,106 @@ class _HomePageState extends State<HomePage> {
       },
       child: Scaffold(
         appBar: AppBar(title: Text("Widgets Interactifs")),
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: _passwordController..text,
-                keyboardType: TextInputType.visiblePassword,
-                decoration: InputDecoration(
-                  labelText: "Saisir votre mail",
-                  hintText: "test@test.com",
-                  suffixIcon: IconButton(
-                    onPressed: (){
-                      setState(() {
-                        obscureText = !obscureText;
-                      });
-                    },
-                    icon: (obscureText) ? Icon(Icons.visibility_off) : Icon(Icons.visibility),
-                  )
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _passwordController..text,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    labelText: "Saisir votre mail",
+                    hintText: "test@test.com",
+                    suffixIcon: IconButton(
+                      onPressed: (){
+                        setState(() {
+                          obscureText = !obscureText;
+                        });
+                      },
+                      icon: (obscureText) ? Icon(Icons.visibility_off) : Icon(Icons.visibility),
+                    )
+                  ),
+                  obscureText: obscureText,
+                  /*
+                  onChanged: (String value){
+                    setState(() {
+                      this.changed = value;
+                    });
+                  },
+                  onSubmitted: (String value){
+                    setState(() {
+                      this.submitted = value;
+                    });
+                  },
+          
+                   */
                 ),
-                obscureText: obscureText,
-                /*
-                onChanged: (String value){
-                  setState(() {
-                    this.changed = value;
-                  });
-                },
-                onSubmitted: (String value){
-                  setState(() {
-                    this.submitted = value;
-                  });
-                },
-
-                 */
-              ),
-              Text("changed = ${changed}"),
-              Text("submitted = ${submitted}"),
-              Divider(),
-              Container(
-                child: Column(
-                  children: checkList(),
+                Text("changed = ${changed}"),
+                Text("submitted = ${submitted}"),
+                Divider(),
+                Container(
+                  child: Column(
+                    children: checkList(),
+                  ),
                 ),
-              ),
-              Divider(),
-              iconRadio,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Radio(
-                    value: RadioChoix.Avion,
-                    onChanged: (RadioChoix? choix){
-                      setState(() {
-                        choixRadio = choix!;
-                        iconRadio = Icon(Icons.airplanemode_active);
-                      });
-                    },
-                    groupValue: choixRadio,
+                Divider(),
+                iconRadio,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Radio(
+                      value: RadioChoix.Avion,
+                      onChanged: (RadioChoix? choix){
+                        setState(() {
+                          choixRadio = choix!;
+                          iconRadio = Icon(Icons.airplanemode_active);
+                        });
+                      },
+                      groupValue: choixRadio,
+                      activeColor: Colors.green,
+                    ),
+                    Text("Avion", style: TextStyle(color: (choixRadio == RadioChoix.Avion) ? Colors.green : Colors.blue),),
+                    Radio(
+                      value: RadioChoix.Bateau,
+                      onChanged: (RadioChoix? choix){
+                        setState(() {
+                          choixRadio = choix!;
+                          iconRadio = Icon(Icons.directions_boat);
+                        });
+                      },
+                      groupValue: choixRadio,
+                      activeColor: Colors.green,
+                    ),
+                    Text("Bateau", style: TextStyle(color: (choixRadio == RadioChoix.Bateau) ? Colors.green : Colors.blue),),
+                    Radio(
+                      value: RadioChoix.Voiture,
+                      onChanged: (RadioChoix? choix){
+                        setState(() {
+                          choixRadio = choix!;
+                          iconRadio = Icon(Icons.directions_car);
+                        });
+                      },
+                      groupValue: choixRadio,
+                      activeColor: Colors.green,
+                    ),
+                    Text("Voiture", style: TextStyle(color: (choixRadio == RadioChoix.Voiture) ? Colors.green : Colors.blue),),
+                  ],
+                ),
+                Divider(),
+                Switch(
+                    value: interrupteur,
+                    inactiveTrackColor: Colors.red,
                     activeColor: Colors.green,
-                  ),
-                  Text("Avion", style: TextStyle(color: (choixRadio == RadioChoix.Avion) ? Colors.green : Colors.blue),),
-                  Radio(
-                    value: RadioChoix.Bateau,
-                    onChanged: (RadioChoix? choix){
+                    onChanged: (bool? b){
                       setState(() {
-                        choixRadio = choix!;
-                        iconRadio = Icon(Icons.directions_boat);
+                        interrupteur = b!;
                       });
-                    },
-                    groupValue: choixRadio,
-                    activeColor: Colors.green,
-                  ),
-                  Text("Bateau", style: TextStyle(color: (choixRadio == RadioChoix.Bateau) ? Colors.green : Colors.blue),),
-                  Radio(
-                    value: RadioChoix.Voiture,
-                    onChanged: (RadioChoix? choix){
-                      setState(() {
-                        choixRadio = choix!;
-                        iconRadio = Icon(Icons.directions_car);
-                      });
-                    },
-                    groupValue: choixRadio,
-                    activeColor: Colors.green,
-                  ),
-                  Text("Voiture", style: TextStyle(color: (choixRadio == RadioChoix.Voiture) ? Colors.green : Colors.blue),),
-                ],
-              )
-            ],
+                    }
+                ),
+                Text(interrupteur?"Pour":"Contre")
+              ],
+            ),
           ),
         ),
       
